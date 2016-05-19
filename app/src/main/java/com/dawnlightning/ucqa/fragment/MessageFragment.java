55b121357d612ce6f8ea82289b1041942aaabb81
fgfgh656 <1558554108@qq.com>
2016-05-19 22:12:43 +0800

    .

--- app/src/main/java/com/dawnlightning/ucqa/fragment/MessageFragment.java
+++ app/src/main/java/com/dawnlightning/ucqa/fragment/MessageFragment.java
@@ -1,4 +1,4 @@
-package com.dawnlightning.ucqa.fragment;
+﻿package com.dawnlightning.ucqa.fragment;
 
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,7 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageView; 
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
