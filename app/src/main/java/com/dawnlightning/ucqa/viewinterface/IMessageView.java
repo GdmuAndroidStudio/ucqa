7bdf67214a20bcbe7fe496ccf547ecda261f26a3
fgfgh656 <1558554108@qq.com>
2016-05-22 22:13:26 +0800

    .

---
+++ app/src/main/java/com/dawnlightning/ucqa/viewinterface/IMessageView.java
@@ -0,0 +1,14 @@
+package com.dawnlightning.ucqa.viewinterface;
+
+import com.dawnlightning.ucqa.base.Actions;
+import com.dawnlightning.ucqa.bean.others.ConsultMessageBean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/5/22.
+ */
+public interface IMessageView {
+    public void showerror(int code, String msg);//显示错误
+    public void showConsultMessageList(List<ConsultMessageBean> list, Actions action);
+}
