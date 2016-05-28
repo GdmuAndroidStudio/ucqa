fa05b8ebb850133df2768b6b7471d6d8c030c786
kkkyo <630304311@qq.com>
2016-05-28 21:21:36 +0800

    commit

--- app/src/main/java/com/dawnlightning/ucqa/adapter/MainAdapter.java
+++ app/src/main/java/com/dawnlightning/ucqa/adapter/MainAdapter.java
@@ -27,6 +27,8 @@ import com.dawnlightning.ucqa.base.Actions;
 import com.dawnlightning.ucqa.bean.others.ConsultClassifyBean;
 import com.dawnlightning.ucqa.bean.others.ConsultMessageBean;
 import com.dawnlightning.ucqa.fragment.MainFragment;
+import com.dawnlightning.ucqa.utils.Options;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.lang.reflect.Field;
@@ -197,21 +199,19 @@ public class MainAdapter extends BaseAdapter {
     static class BannerViewHolder extends ViewHolder {
 
         private ConvenientBanner convenientBanner;
+        private ImageLoader imageLoader = ImageLoader.getInstance();
+        private DisplayImageOptions options = Options.getListOptions();
         private ArrayList<Integer> localImages = new ArrayList<Integer>();
         private List<String> networkImages;
-        private String[] images = {"http://img2.imgtn.bdimg.com/it/u=3093785514,1341050958&fm=21&gp=0.jpg",
-                "http://img2.3lian.com/2014/f2/37/d/40.jpg",
-                "http://d.3987.com/sqmy_131219/001.jpg",
-                "http://img2.3lian.com/2014/f2/37/d/39.jpg",
-                "http://www.8kmm.com/UploadFiles/2012/8/201208140920132659.jpg",
-                "http://f.hiphotos.baidu.com/image/h%3D200/sign=1478eb74d5a20cf45990f9df460b4b0c/d058ccbf6c81800a5422e5fdb43533fa838b4779.jpg",
-                "http://f.hiphotos.baidu.com/image/pic/item/09fa513d269759ee50f1971ab6fb43166c22dfba.jpg"
+        private String[] images = {"http://img2.imgtn.bdimg.com/it/u=1514107744,1001213722&fm=21&gp=0.jpg",
+                "http://b.hiphotos.baidu.com/image/h%3D200/sign=2a1d57fa08b30f242a9aeb03f894d192/4d086e061d950a7b539cb25f0dd162d9f3d3c984.jpg",
+                "http://img3.imgtn.bdimg.com/it/u=882887467,2910558374&fm=21&gp=0.jpg"
         };
 
         public BannerViewHolder(final Context context, View view) {
             super(view);
             convenientBanner = (ConvenientBanner) view.findViewById(R.id.banner);
-            loadLocalImages();
+//            loadLocalImages();
 //            convenientBanner.setPages(
 //                    new CBViewHolderCreator<LocalImageHolderView>() {
 //                        @Override
@@ -308,8 +308,7 @@ public class MainAdapter extends BaseAdapter {
 
             @Override
             public void UpdateUI(Context context, int position, String data) {
-                imageView.setImageResource(R.mipmap.ic_default_adimage);
-                ImageLoader.getInstance().displayImage(data, imageView);
+                imageLoader.displayImage(data, imageView,options);
             }
         }
     }
